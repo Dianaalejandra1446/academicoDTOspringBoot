320589b6b0bc40779a147c8e8042d24a7bfe7299
Dianaalejandra1446 <dianaleja04@hotmail.com>
2024-03-20 20:43:52 -0500

    Last update

--- academico/src/main/java/com/universidad/academico/Configuracion/EstudianteDTOConverte.java
+++ academico/src/main/java/com/universidad/academico/Configuracion/EstudianteDTOConverte.java
@@ -1,25 +1,29 @@
+// Indica el paquete al que pertenece esta clase
 package com.universidad.academico.Configuracion;
 
+// Importa las clases necesarias
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
-
 import com.universidad.academico.Dto.EstudianteDTO;
 import com.universidad.academico.Repository.entidadesDocumentos.EstudianteEntity;
 
+// Indica que esta clase es un componente de Spring y será administrada por el contenedor de Spring
 @Component
 public class EstudianteDTOConverte {
-    private ModelMapper dbm;
+    private ModelMapper dbm; // Instancia de ModelMapper para realizar las conversiones
 
-    public EstudianteDTO converEstudianteDTO(EstudianteEntity estudiante){
-        EstudianteDTO estudianteDTO=dbm.map(estudiante, EstudianteDTO.class);
-        if (estudiante.getMatricula()!=null) {
-            estudianteDTO.setMatriculaId(estudiante.getMatricula().getId());
+    // Método para convertir un EstudianteEntity a EstudianteDTO
+    public EstudianteDTO converEstudianteDTO(EstudianteEntity estudiante) {
+        EstudianteDTO estudianteDTO = dbm.map(estudiante, EstudianteDTO.class); // Mapea los campos de estudiante a estudianteDTO
+        if (estudiante.getMatricula() != null) {
+            estudianteDTO.setMatriculaId(estudiante.getMatricula().getId()); // Si hay una matrícula asociada, establece su ID en el DTO
         }
-        estudianteDTO.setUniversidadId(estudiante.getUniversidad().getId());
-        return estudianteDTO;
-
+        estudianteDTO.setUniversidadId(estudiante.getUniversidad().getId()); // Establece el ID de la universidad en el DTO
+        return estudianteDTO; // Devuelve el objeto EstudianteDTO convertido
     }
-    public EstudianteEntity converEstudianteEntity(EstudianteDTO estudianteDTO){
-        return dbm.map(estudianteDTO,EstudianteEntity.class);
+
+    // Método para convertir un EstudianteDTO a EstudianteEntity
+    public EstudianteEntity converEstudianteEntity(EstudianteDTO estudianteDTO) {
+        return dbm.map(estudianteDTO, EstudianteEntity.class); // Mapea los campos de estudianteDTO a EstudianteEntity y devuelve el resultado
     }
 }
--- academico/src/main/java/com/universidad/academico/Configuracion/MatriculaDTOConverte.java
+++ academico/src/main/java/com/universidad/academico/Configuracion/MatriculaDTOConverte.java
@@ -1,25 +1,28 @@
+// Indica el paquete al que pertenece esta clase
 package com.universidad.academico.Configuracion;
 
+// Importa las clases necesarias
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import com.universidad.academico.Dto.MatriculaDTO;
 import com.universidad.academico.Repository.entidadesDocumentos.MatriculaEntity;
 
+// Indica que esta clase es un componente de Spring y será administrada por el contenedor de Spring
 @Component
 public class MatriculaDTOConverte {
-    @Autowired
-    private ModelMapper dbm;
+    @Autowired // Inyección de dependencia de Spring que proporciona una instancia de ModelMapper
+    private ModelMapper dbm; // Instancia de ModelMapper para realizar las conversiones
 
-    public MatriculaDTO conveMatriculaDTO(MatriculaEntity matricula){
-
-        MatriculaDTO matriculaDTO = dbm.map(matricula, MatriculaDTO.class);
-        matriculaDTO.setEstudianteId(matricula.getId());
-        return matriculaDTO;
+    // Método para convertir una MatriculaEntity a MatriculaDTO
+    public MatriculaDTO conveMatriculaDTO(MatriculaEntity matricula) {
+        MatriculaDTO matriculaDTO = dbm.map(matricula, MatriculaDTO.class); // Mapea los campos de matricula a matriculaDTO
+        matriculaDTO.setEstudianteId(matricula.getId()); // Establece el ID del estudiante en el DTO
+        return matriculaDTO; // Devuelve el objeto MatriculaDTO convertido
     }
 
-    public MatriculaEntity converMatriculaEntity(MatriculaDTO matriculaDTO){
-        return dbm.map(matriculaDTO, MatriculaEntity.class);
+    // Método para convertir una MatriculaDTO a MatriculaEntity
+    public MatriculaEntity converMatriculaEntity(MatriculaDTO matriculaDTO) {
+        return dbm.map(matriculaDTO, MatriculaEntity.class); // Mapea los campos de matriculaDTO a MatriculaEntity y devuelve el resultado
     }
 }
