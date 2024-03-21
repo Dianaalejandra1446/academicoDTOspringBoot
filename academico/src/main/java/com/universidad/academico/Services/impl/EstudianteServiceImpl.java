bf9121d31e3c38d852916d5e0a288d06453f3577
Dianaalejandra1446 <dianaleja04@hotmail.com>
2024-03-20 20:55:06 -0500

    Documentacion DTO

--- academico/src/main/java/com/universidad/academico/Services/impl/EstudianteServiceImpl.java
+++ academico/src/main/java/com/universidad/academico/Services/impl/EstudianteServiceImpl.java
@@ -16,22 +16,26 @@ import jakarta.transaction.Transactional;
 
 @Service
 public class EstudianteServiceImpl implements EstudianteServices {
-    @Autowired
+    @Autowired // Anotación que indica que el contenedor de Spring debe inyectar una instancia de EstudianteRepository en este campo
     private EstudianteRepository estudianteRepository;
-
-    @Autowired
+    
+    @Autowired // Anotación que indica que el contenedor de Spring debe inyectar una instancia de EstudianteDTOConverte en este campo
     private EstudianteDTOConverte convert;
-
-    @Override
-    @Transactional
+    
+    @Override // Anotación para indicar que este método sobrescribe un método de la clase padre
+    @Transactional // Anotación que indica que esta operación debe ser realizada dentro de una transacción
     public List<EstudianteDTO> finAll() {
-        List<EstudianteEntity> estudianteEntities= (List<EstudianteEntity>) estudianteRepository.findAll();
-        List<EstudianteDTO> estudianteDTO=new ArrayList<>();
-
-            for (EstudianteEntity estudiante : estudianteEntities) {
-                estudianteDTO.add(convert.converEstudianteDTO(estudiante));
-            }
-            return estudianteDTO;
+        // Se obtienen todas las entidades de estudiantes de la base de datos a través del repositorio de estudiantes
+        List<EstudianteEntity> estudianteEntities = (List<EstudianteEntity>) estudianteRepository.findAll();
+        
+        List<EstudianteDTO> estudianteDTO = new ArrayList<>(); //  Creamos una lista para almacenar los objetos EstudianteDTO
+    
+        // Se itera sobre cada entidad de estudiante y se convierte a EstudianteDTO utilizando el objeto convert de tipo EstudianteDTOConverte
+        for (EstudianteEntity estudiante : estudianteEntities) {
+            estudianteDTO.add(convert.converEstudianteDTO(estudiante)); // Se agrega el EstudianteDTO convertido a la lista
+        }
+    
+        return estudianteDTO; // Se devuelve la lista de EstudianteDTO
     }
     
 }
--- academico/src/main/java/com/universidad/academico/Services/impl/MatriculaServicieImpl.java
+++ academico/src/main/java/com/universidad/academico/Services/impl/MatriculaServicieImpl.java
@@ -20,12 +20,15 @@ import com.universidad.academico.Services.MatriculaServices;
 public class MatriculaServicieImpl implements MatriculaServices {
 
     @Autowired
+    // Traemos el repository de matriculas
     private MatriculasRepository matriculasRepository;
 
     @Autowired
+    // Traemos de repository de estudiante
     private EstudianteRepository estudianteRepository;
 
     @Autowired
+    // Traemos la MatriculaDTO
     private MatriculaDTOConverte convert;
 
     @Override
@@ -34,21 +37,30 @@ public class MatriculaServicieImpl implements MatriculaServices {
         Optional<EstudianteEntity> estudianteOptional = estudianteRepository.findById(matricula.getEstudianteId());
 
         if (estudianteOptional.isPresent()) {
+            // Aca convertimos en entity a DTO si hay datos encontrados
             MatriculaEntity matriculaEntity = convert.converMatriculaEntity(matricula);
             matriculaEntity.setEstudiante(estudianteOptional.get());
             matriculasRepository.save(matriculaEntity);
+            // Devolvemos la matricula convertida en DTO
             return convert.conveMatriculaDTO(matriculaEntity);
         }
         return null;
     }
 
-    @Override
-    @Transactional(readOnly = true)
+    @Override // Anotación para indicar que este método sobrescribe un método de la clase padre
+    @Transactional(readOnly = true) // Anotación que indica que esta operación es de solo lectura y debe estar dentro de una transacción
+    // Traer todos los datos
     public List<MatriculaDTO> findAll() {
+        // Se obtienen todas las matrículas de la base de datos a través del repositorio de matrículas
         List<MatriculaEntity> matriculasEntity = (List<MatriculaEntity>) matriculasRepository.findAll();
+        
+        // Se convierten las matrículas de tipo MatriculaEntity a MatriculaDTO utilizando una expresión lambda
+        // y se mapean utilizando el objeto convert de tipo MatriculaDTOConverte
+        // Finalmente, se recopilan en una lista de MatriculaDTO utilizando el método collect del stream
         return matriculasEntity.stream()
                                 .map(matricula -> convert.conveMatriculaDTO(matricula))
                                 .collect(Collectors.toList());
     }
+    
 }
 
